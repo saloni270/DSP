baa4881ce2b1e5397e96d2a2d0b2dccc759e5a09
hetvi <hetvikaswala151@gmail.com>
2024-08-04 20:52:39 -0400

    implementation of the DJB2 hash function

--- DSPRO/DSPRO.cpp
+++ DSPRO/DSPRO.cpp
@@ -15,3 +15,13 @@ typedef struct Parcel {
 typedef struct {
     Parcel* root;
 } HashTable;
+
+
+unsigned long hashFunction(const char* str) {
+    unsigned long hash = 5381;
+    int currentChar;
+    while ((currentChar = *str++))
+        hash = ((hash << 5) + hash) + currentChar; // hash * 33 + c
+    return hash % HASH_TABLE_SIZE;
+}
+
