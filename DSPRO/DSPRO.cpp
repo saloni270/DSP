35b68000aa593341e403e32bc9e5d4f0e562c7f9
Saloni <salonipatel501@gmail.com>
2024-08-04 20:05:46 -0400

    defined the hashtable and maxninum number length, along with that also created structure.

--- DSPRO/DSPRO.cpp
+++ DSPRO/DSPRO.cpp
@@ -1,1 +1,17 @@
-//CHECKING GITHUB COMMITS
+#include <string.h>
+
+
+#define hashTableSize 127
+#define maximumCountryLength 20
+
+typedef struct Parcel {
+    char country[maximumCountryLength + 1];
+    int weight;
+    float value;
+    struct Parcel* left;
+    struct Parcel* right;
+} Parcel;
+
+typedef struct {
+    Parcel* root;
+} HashTable;
