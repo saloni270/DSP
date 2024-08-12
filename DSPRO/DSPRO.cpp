d6614c09ab03a598793471e7e2c24ef563f3bc55
hetvi <hetvikaswala151@gmail.com>
2024-08-12 14:33:23 -0400

    all the function comments are done.

--- DSPRO/DSPRO.cpp
+++ DSPRO/DSPRO.cpp
@@ -22,7 +22,9 @@ typedef struct {
     Parcel* root; // pointer to the root of a BST.
 } HashTable;
 
-// DJB2 hash function for generating hash values.
+// function name: hashFunction
+// data type: unsigned long
+// description: A DJB2 algorithm for hash table  provided the string.
 unsigned long hashFunction(const char* str) {
     unsigned long hash = 5381;
     int currentChar;
@@ -30,6 +32,10 @@ unsigned long hashFunction(const char* str) {
         hash = ((hash << 5) + hash) + currentChar; // hash * 33 + currentChar
     return hash % hashTableSize;
 }
+
+// function name: createHashTable
+// data type : HashTable*
+// description: Creating and initialising a new hashtable of the given size.
 HashTable* createHashTable() {
     // Allocating the memory for the hash table, which is an array of `HashTable` structures.
     HashTable* table = (HashTable*)malloc(sizeof(HashTable)* hashTableSize);
@@ -37,13 +43,16 @@ HashTable* createHashTable() {
         printf("Memory allocation failed.\n");
         exit(1);
     }
-    // Checking  an empty BST at the each hash index, and also intializing each bucket root to Null
+    // Checking  an empty BST at the each hash index, and also intializing each bucket root to Null.
     for (int i = 0; i < hashTableSize; i++) {
         table[i].root = NULL;
     }
     return table; // returing table (pointer ) to created the hash table.
 }
 
+// function name: createParcel
+// data type: Parcel*
+// description: Creates a new Parcel structure given the country to delivery the parcels weight and total parcels value.
 Parcel* createParcel(const char* country, int weight, float value) {
     Parcel* newParcel = (Parcel*)malloc(sizeof(Parcel));
     if (newParcel == NULL) {
@@ -59,6 +68,10 @@ Parcel* createParcel(const char* country, int weight, float value) {
     newParcel->right = NULL;
     return newParcel;// Returning the pointer to the newly created parcel.
 }
+
+// function name: insertParcelToTree
+// data type: void
+// description: Add a Parcel to a binary search tree using a weight-based insert.
 void insertParcelToTree(Parcel** root, Parcel* newParcel) {
     if (*root == NULL) {// placing the new parcel there, if the current node is Null.
         *root = newParcel;
@@ -72,15 +85,21 @@ void insertParcelToTree(Parcel** root, Parcel* newParcel) {
         insertParcelToTree(&((*root)->right), newParcel);// inserting into right-subtree, recursively.
     }
 }
+
+// function name: insertParcel
+// data type: void
+// description: Add a Parcel into the hash table, placing in the correct BST (binary tree) according to the country name's hash index.
 void insertParcel(HashTable* table, const char* country, int weight, float value) {
     unsigned long hashIndex = hashFunction(country);// calculating the hash-table
     Parcel* newParcel = createParcel(country, weight, value);
     insertParcelToTree(&(table[hashIndex].root), newParcel);// inserting new parcel to calculated hash index.
 
 }
-// country is not found, if the current node is Null.
+// function name: isCountryInTree
+// data type: int
+// description: Checks if a country is present in a BST (Binary Tree)
 int isCountryInTree(Parcel* root, const char* country) {
-    if (root == NULL) {
+    if (root == NULL) {// country is not found, if the current node is Null.
         return 0;
     }
     // if the country is found it will return 1 as a true.
@@ -90,12 +109,16 @@ int isCountryInTree(Parcel* root, const char* country) {
     // searching for the left or right sub-tree.
     return isCountryInTree(root->left, country) || isCountryInTree(root->right, country);
 }
-
+// function name: isCountryInHashTable
+// data type: int
+// description: Check if a country exists in the hash table.
 int isCountryInHashTable(HashTable* table, const char* country) {
     unsigned long hashIndex = hashFunction(country);// calculating the hash-table
     return isCountryInTree(table[hashIndex].root, country);// if country exits, it calculated the hash index.
 }
- 
+// function name: displayParcels
+// data type: void
+// description: Present all Parcels in BST using in-order traversal.
 void displayParcels(Parcel* root) {
     if (root != NULL) { // proceed with traversal if the current node is not Null.
         displayParcels(root->left);
@@ -103,7 +126,9 @@ void displayParcels(Parcel* root) {
         displayParcels(root->right);
     }
 }
-
+// function name: displayParcelsForCountry
+// data type: void
+// description: Retrieving  all Parcels belonging to a specific country from the hash table
 void displayParcelsForCountry(HashTable* table, const char* country) {
     if (!isCountryInHashTable(table, country)) {// checks if the country exists in the hash table.
         printf("No parcels found for %s.\n", country);
@@ -114,6 +139,9 @@ void displayParcelsForCountry(HashTable* table, const char* country) {
     printf("Parcels for %s (Hash Index: %lu):\n", country, hashIndex);
     displayParcels(table[hashIndex].root);// displaying all parcels and calculates the hash index.
 }
+// function name: displayParcelsForCountry
+// data type: void
+// description: Retrieving all Parcels belonging to a specific country from the hash table.
 int hasParcelsByWeight(Parcel* root, int weight, int showGreaterThan) {
     if (root == NULL) {// there are no more parcel, if the current node is null.
         return 0;
@@ -125,10 +153,11 @@ int hasParcelsByWeight(Parcel* root, int weight, int showGreaterThan) {
     else if (!showGreaterThan && root->weight < weight) {
         return 1; // returning true if it does.
     }
-
     return hasParcelsByWeight(root->left, weight, showGreaterThan) ||  hasParcelsByWeight(root->right, weight, showGreaterThan);
 }
-
+// function name: displayParcelsByWeight
+// data type: void
+// description: List all Parcels within the BST with a given weight limits. (less than or greater than).
 void displayParcelsByWeight(Parcel* root, int weight, int showGreaterThan) {
     if (root != NULL) {// go with traversal if the current node is null.
         displayParcelsByWeight(root->left, weight, showGreaterThan);
@@ -142,7 +171,9 @@ void displayParcelsByWeight(Parcel* root, int weight, int showGreaterThan) {
         displayParcelsByWeight(root->right, weight, showGreaterThan);// displaying the parcels in the right sub-tree.
     }
 }
-
+// function name: displayParcelsForCountryByWeight
+// data type: void
+// description: Output all Parcels that are linked with the country mentioned in the hash table and satisfy the weight condition.
 void displayParcelsForCountryByWeight(HashTable* table, const char* country, int weight) {
     if (!isCountryInHashTable(table, country)) {// Check if the country exists in the hash table.
         printf("No parcels found for %s.\n", country);
@@ -157,7 +188,6 @@ void displayParcelsForCountryByWeight(HashTable* table, const char* country, int
     else {
         displayParcelsByWeight(table[hashIndex].root, weight, 0);
     }
-
     printf("Parcels for %s with weight greater than %d:\n", country, weight);// informaning the user about weight creteria.
     if (!hasParcelsByWeight(table[hashIndex].root, weight, 1)) {
         printf("None\n");
@@ -166,7 +196,9 @@ void displayParcelsForCountryByWeight(HashTable* table, const char* country, int
         displayParcelsByWeight(table[hashIndex].root, weight, 1);
     }
 }
-
+// function name: calculateTotalLoadAndValuation
+// data type: void
+// description: Calculates total weight and total value of all Parcels in the BST and also it changes the input pointers.
 void calculateTotalLoadAndValuation(Parcel* root, int* totalLoad, float* totalValuation) {
     if (root != NULL) {
         calculateTotalLoadAndValuation(root->left, totalLoad, totalValuation);// processing with the calculation, if the node is null.
@@ -175,6 +207,9 @@ void calculateTotalLoadAndValuation(Parcel* root, int* totalLoad, float* totalVa
         calculateTotalLoadAndValuation(root->right, totalLoad, totalValuation);
     }
 }
+// function name: displayTotalLoadAndValuation
+// data type: void
+// description: Outputs total weight and total value of all Parcels of a particular country in the hash table.
 void displayTotalLoadAndValuation(HashTable* table, const char* country) {
     if (!isCountryInHashTable(table, country)) { // Checking if the country exists in the hash table.
         printf("No parcels found for %s.\n", country);
@@ -188,7 +223,9 @@ void displayTotalLoadAndValuation(HashTable* table, const char* country) {
     printf("Total load for %s: %d grams\n", country, totalLoad);
     printf("Total valuation for %s: %.2f dollars\n", country, totalValuation);
 }
-
+// function name: findCheapestParcel
+// data type: Parcel*
+// description: Finds and returns the Parcel with the lowest value in the BST.
 Parcel* findCheapestParcel(Parcel* root) {
     if (root == NULL) {// if the current node is null return null and say no parcel found. 
         return NULL;
@@ -205,7 +242,9 @@ Parcel* findCheapestParcel(Parcel* root) {
     }
     return cheapest;
 }
-
+// function name: findMostExpensiveParcel
+// data type: Parcel*
+// description: A method that gets the Parcel with the highest value in the BST.
 Parcel* findMostExpensiveParcel(Parcel* root) {
     if (root == NULL) {// if the current node is null return null and say no parcel found.
         return NULL;
@@ -222,7 +261,9 @@ Parcel* findMostExpensiveParcel(Parcel* root) {
     }
     return mostExpensive;
 }
-
+// function name: displayCheapestAndMostExpensiveParcels
+// data type: void
+// description: Displays the Parcel with the lowest and highest value associated with a specific country in the hash table.
 void displayCheapestAndMostExpensiveParcels(HashTable* table, const char* country) {
     if (!isCountryInHashTable(table, country)) { // Checking if the country exists in the hash table.
         printf("No parcels found for %s.\n", country);
@@ -239,8 +280,9 @@ void displayCheapestAndMostExpensiveParcels(HashTable* table, const char* countr
         printf("Most expensive parcel for %s: Weight: %d, Value: %.2f\n", country, mostExpensive->weight, mostExpensive->value);
     }
 }
-
-
+// function name: findLightestParcel
+// data type : Parcel*
+// description: Find and return Parcel in the BST with the smallest weight.
 Parcel* findLightestParcel(Parcel* root) {
     if (root == NULL) {// if the current node is null return null and say no parcel found.
         return NULL;
@@ -257,7 +299,9 @@ Parcel* findLightestParcel(Parcel* root) {
     }
     return lightest;
 }
-
+// function name: findHeaviestParcel
+// data type: Parcel*
+// description: Find and return the Parcel with the highest weight in the BST.
 Parcel* findHeaviestParcel(Parcel* root) {
     if (root == NULL) {
         return NULL;
@@ -274,7 +318,9 @@ Parcel* findHeaviestParcel(Parcel* root) {
     }
     return heaviest;
 }
-
+// function name: displayLightestAndHeaviestParcels
+// data type: void
+// description: Displays the Parcel with the least and highest weight linked with a given country in the hash table.
 void displayLightestAndHeaviestParcels(HashTable* table, const char* country) {
     if (!isCountryInHashTable(table, country)) {// Checking if the country exists in the hash table.
         printf("No parcels found for %s.\n", country);
@@ -291,9 +337,9 @@ void displayLightestAndHeaviestParcels(HashTable* table, const char* country) {
         printf("Heaviest parcel for %s: Weight: %d, Value: %.2f\n", country, heaviest->weight, heaviest->value);
     }
 }
-
-
-
+// function name: freeTree
+// data type: void
+// description: Frees the memory for a BST of Parcels.
 void freeTree(Parcel * root) {
     if (root != NULL) {// if the current node is not null, then proceed with the free uping the memory.
         freeTree(root->left);// free the left sub-tree
@@ -301,22 +347,24 @@ void freeTree(Parcel * root) {
         free(root);// free the current root from the struct of the parcel.
     }
 }
-
+// function name: freeHashTable
+// data type: void
+// description: Frees the memory allocated for the entire hash table, including all BSTs within it.
 void freeHashTable(HashTable* table) {
     for (int i = 0; i < hashTableSize; i++) {// for going to each bucket in the has table.
         freeTree(table[i].root);// free the hash index from the BST.
     }
     free(table);// free the memory allocated hash table.
 }
-
-// loading the country name, weight and the value from txt file into the hash table.
+// function name: loadData
+// data type : void
+// description: Reads parcel data in from a text file to the hash table.
 void loadData(HashTable* table, const char* filename) {
     FILE* file = fopen(filename, "r");// function used to open the file in the read only mode.
     if (file == NULL) {
         printf("Error opening file.\n");
         exit(1);
     }
-
     char line[256];// storing the each line read data from the txt file.
     char country[maximumCountryLength + 1];
     int weight;// storing the value 
@@ -328,19 +376,20 @@ void loadData(HashTable* table, const char* filename) {
             insertParcel(table, country, weight, value);// inserting the parsed information into the hash table.
         }
     }
-
     fclose(file); // function used to close the txt file after reading all the data.
 }
 
-// function to help the user to choose the menu from given choice.
+// function name: displayMenu
+// data type: void
+// description: Displays the user menu with available options.
 void displayMenu() {
     printf("Menu:\n");
-    printf("1. Enter country name and display all parcels\n");
-    printf("2. Enter country and weight pair to display parcels\n");
-    printf("3. Display total parcel load and valuation for a country\n");
-    printf("4. Display cheapest and most expensive parcels for a country\n");
-    printf("5. Display lightest and heaviest parcels for a country\n");
-    printf("6. Exit\n");
+    printf("1. Enter country name and display all parcels details:\n");
+    printf("2. Enter country and weight pair:\n");
+    printf("3. Display total parcel load and valuation for the country:\n");
+    printf("4. Enter the country name and display cheapest and most expensive parcel's details.\n");
+    printf("5. Enter the country name and display lightest and heaviest parcels for a country.\n");
+    printf("6. Exit the application.\n");
 }
 
 int main() {
